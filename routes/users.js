acff307ca9cafc8ab3097042098ba85dd840a088
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-16 15:30:27 -0400

    Add GET and POST routes for user sign up

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.get("/sign-up", userController.userCreateGet);
+
+router.post("/sign-up", userController.userCreatePost);
 
 module.exports = router;
