8d28335ed7e81d91f95d1d4627fad236b07ee75c
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-18 08:48:44 -0400

    Restrict new message form to logged in users

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const permissions = require("../middleware/permissions");
 
 // Create Message
-exports.messageCreateGet = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message Create GET");
-});
+exports.messageCreateGet = [
+  // Check permissions
+  permissions.isLoggedIn,
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Message Create GET");
+  }),
+];
 
 exports.messageCreatePost = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Message Create POST");
