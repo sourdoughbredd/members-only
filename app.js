4e380bac2502d8a46e2263e789227fb54c18119f
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-18 13:59:16 -0400

    Add rate limiter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const authenticateJWT = require("./middleware/authMiddleware");
+const rateLimit = require("express-rate-limit").rateLimit;
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -28,6 +29,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Apply the rate limiting middleware to all requests.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+app.use(limiter);
 
 // Authenticate user (sets res.locals.isAuthenticated)
 app.use(authenticateJWT);
