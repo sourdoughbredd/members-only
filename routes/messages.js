cad466c8455ee96bd1ba6ab17cc498bbcaee01e1
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-18 10:18:55 -0400

    Create message board route

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const messageController = require("../controllers/messageController");
 
+// Display message board
+router.get("/", messageController.messageBoardGet);
+
 // Create message
 router.get("/create", messageController.messageCreateGet);
 router.post("/create", messageController.messageCreatePost);
