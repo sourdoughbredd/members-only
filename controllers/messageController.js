f95edbf2152c9337d35b15db1c868e3f1df71880
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-18 09:44:10 -0400

    Implement basic create message form

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,15 +1,38 @@
 const asyncHandler = require("express-async-handler");
 const permissions = require("../middleware/permissions");
+const { body, validationResult } = require("express-validator");
+
+const Message = require("../models/message");
 
 // Create Message
 exports.messageCreateGet = [
-  // Check permissions
   permissions.isLoggedIn,
   asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Message Create GET");
+    res.render("message-form", {});
   }),
 ];
 
-exports.messageCreatePost = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message Create POST");
-});
+exports.messageCreatePost = [
+  permissions.isLoggedIn,
+  body("title", "Title is required").trim().notEmpty(),
+  body("text", "Message is required").trim().notEmpty(),
+  asyncHandler(async (req, res, next) => {
+    // Extract validation results
+    const errors = validationResult(req).array();
+
+    const message = new Message({
+      user: req.user,
+      title: req.body.title,
+      text: req.body.text,
+    });
+
+    if (errors.length > 0) {
+      res.render("message-form", { message, errors });
+      return;
+    }
+
+    // Post the message
+    await message.save();
+    res.redirect("/");
+  }),
+];
